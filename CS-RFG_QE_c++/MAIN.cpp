52f977600da72d19cecbcdd4a1cb4656fc548fe7
Limoncito <73139277+ornitorrinco101@users.noreply.github.com>
2025-02-03 23:28:19 +0100

    Add files via upload

---
+++ CS-RFG_QE_c++/MAIN.cpp
@@ -0,0 +1,250 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <complex>
+#include <cstdarg>
+
+using namespace std;
+
+#include "Constants.h"
+#include "Leptonic.h"
+#include "Hadronic.h"
+
+int main()
+{
+
+  // An input file is opened   
+  ifstream in("Input.txt"); // Input file 
+
+  int process; // 0 EM; 1 CC
+  in >> process;
+  in.ignore(1000, '\n');  // line skipper
+    
+  int A, Z, pF; // A: mass number; Z: atomic number; Fermi momentum (MeV)
+  in >> A >> Z >> pF;
+  in.ignore(1000, '\n');  // line skipper
+
+  double Ei; // incoming lepton energy (MeV)
+  in >> Ei;
+  in.ignore(1000, '\n');
+
+  double thetaf_deg; // Scattering angle (degrees)
+  in >> thetaf_deg;
+  in.ignore(1000, '\n');
+  
+    double thetaf = thetaf_deg*Pi/180.,    costheta_f = cos(thetaf),    sintheta_f=sin(thetaf);
+
+  int Helicity; // 1 antineutrinos; -1 neutrinos
+  in >> Helicity; 
+  in.ignore(1000, '\n');
+
+  in.close();
+  // // // // // // // // // 
+
+  
+  // // // building the 4-vectors of incoming lepton and defining the reference frame // // //   
+  double ki=Ei; //3-mom of initial lepton (ultrarrelativistic approximation)
+  double Ki[4] = {Ei,0,0,ki}; //initial lepton 
+  double Ki_c[4] = {Ei,0,0,-ki};
+  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //   
+  
+  // // // OUTPUT FILE // // //
+  int iEi=Ei;
+  int itheta=thetaf_deg;
+  ofstream out; //name of the output unit in the code 
+  out.open("Ei"+to_string(iEi)+"_th"+to_string(itheta)+".out"); //name of the output file
+  out.precision(4); //number of digits in the printed numbers 
+  // // // // // // // // // //
+  
+  for(double w=1; w<=Ei; w+=1)
+  {
+
+    cout << "w= " << w << endl;
+    
+    
+    // // final lepton // //
+    double Ef = Ei-w;       //if(Ef >= Ei || Ef < 0){continue;}
+    double kf2 = pow(Ef,2) - pow(muonmass,2);                 if(kf2 < 0){continue;}  // "continue" breaks the flow and sends it back to the next value of the loop (w in our case)  
+    double kf=sqrt(kf2);
+    double Kf[4] = {Ef,kf*sintheta_f,0,kf*costheta_f};
+    double Kf_c[4] = {Kf[0],-Kf[1],-Kf[2],-Kf[3]};
+    // // // // // // // // 
+
+    // // exchanged boson // //
+    double Q[4];
+    for(int i=0; i<4; i++)
+    {
+      Q[i] = Ki[i] - Kf[i];
+    }
+    if(Q[0] < 0){continue;} //the energy must be positive
+    
+    double Q2 = Q[0]*Q[0] - Q[1]*Q[1] - Q[2]*Q[2] -Q[3]*Q[3];   if(Q2 >= 0){continue;} //Q2 must be negative
+    
+    double q2 = w*w - Q2;  //magnitude of the 3-momentum transfer (squared)  
+    if( q2 <= 0 ){ continue; }
+    double q = sqrt(q2);
+    // // // // // // // // 
+    
+    
+    double d3sig=0; //declaration and initialization of the cross section at zero
+    
+    double thetaN_deg_step=0.5;
+    for(double thetaN_deg=0; thetaN_deg<=180; thetaN_deg+=thetaN_deg_step)
+    {
+      double thetaN=thetaN_deg*Pi/180;  
+        if(thetaN_deg==0){thetaN=0.00001;}
+        if(thetaN_deg==180){thetaN=Pi-0.00001;}
+      
+      double thetaN_step=thetaN_deg_step*Pi/180;
+      double costhetaN = cos(thetaN),  sinthetaN = sin(thetaN);
+      
+      
+      double phiN_deg_step=0.5;
+      double d4sig=0;
+      for(double phiN_deg=0; phiN_deg<=360; phiN_deg+=phiN_deg_step)
+      {
+        double phiN=phiN_deg*Pi/180;
+        double phiN_step=phiN_deg_step*Pi/180;
+        double cosphiN = cos(phiN),  sinphiN = sin(phiN);
+
+        
+        // // // // // // // // //
+        // // final nucleon // //
+
+        // // direction of the final nucleon // //
+        double hpNx = sinthetaN*cosphiN, 
+               hpNy = sinthetaN*sinphiN, 
+               hpNz = costhetaN;
+        // // // // // // // // // // // // // // 
+        
+        double costheta_qN = (Q[1]*hpNx + Q[2]*hpNy + Q[3]*hpNz)/q;
+        
+        double AA = Q2/(2*w),   BB = q*costheta_qN/w; 
+        double deter = AA*AA + (BB*BB - 1)*MN2;               if(deter<0){continue;}
+        
+        double pN = ( -AA*BB + sqrt(deter) ) / ( BB*BB - 1 );    if(pN<0){continue;}
+        
+        // // Pauli blocking // //
+        if(pN < pF){continue;}
+        // // // // // // // // //
+        
+        double PN[4]={ sqrt(pN*pN + MN2), pN*hpNx, pN*hpNy, pN*hpNz };
+        if(PN[0] < MN){continue;} //the energy must be larger than the nucleon mass
+        // // // // // // // // // // 
+        // // // // // // // // // //
+        
+        
+        // // initial nucleon // //
+        double P[4];
+        for(int i=0; i<4; i++)
+        {
+          P[i] = PN[i] - Q[i];
+        }
+        double p = sqrt(pow(P[1],2) + pow(P[2],2) + pow(P[3],2));
+
+        if( p > pF ){continue;} // above the Fermi level, then not a bound nucleon in the RFG model 
+        // // // // // // // // // //
+
+        // // Lepton tensor // // 
+        complex<double> Lmn[4][4]; //~MeV^2
+        Lmunu(process, Helicity, Ki_c, Kf_c, Lmn); //output is Lmn (with indices down), the rest is input 
+        // // // // // // // // // // //
+
+        // // Contraction // //
+        complex<double> LmnHmn=0;
+        
+        if(process==0){ //EM interaction
+          // // Hadron tensor // // 
+          complex<double> Hmn_p[4][4]={}; //~dimensionless
+          Hmunu(process, 1, P, PN, Hmn_p); //output is Hmn, the rest is input
+          complex<double> Hmn_n[4][4]={}; //~dimensionless
+          Hmunu(process, 0, P, PN, Hmn_n); //output is Hmn, the rest is input
+          // // // // // // // // // // //
+          
+          for(int i=0; i<4; i++)
+          {
+            for(int j=0; j<4; j++)
+            {
+              
+              LmnHmn += Lmn[i][j] * ( double(Z)*Hmn_p[i][j] + double(A-Z)*Hmn_n[i][j] ); //~MeV^2
+              
+            }
+          }  
+        }
+        else if(process==1)
+        { 
+          // // Hadron tensor // // 
+          complex<double> Hmn[4][4]={}; //~dimensionless
+          Hmunu(process, 0, P, PN, Hmn); //output is Lmn, the rest is input
+          // // // // // // // // // // //
+          
+          double NN;
+          if(Helicity==-1){NN=double(A-Z);} //neutrinos interact with neutrons
+          else if(Helicity==1){NN=double(Z);} //antineutrinos interact with protons
+          
+          for(int i=0; i<4; i++)
+          {
+            for(int j=0; j<4; j++)
+            {
+              
+              LmnHmn += Lmn[i][j] * NN *Hmn[i][j] ; //~MeV^2
+              
+            }
+          }            
+        }
+        // // // // // // // // // 
+        
+        double FF; // factor from the exchanged boson (photon or W)
+        if(process == 1){ FF = pow(G_Fermi*Cabibbo/sqrt(2.),2); } //Weak Charged-Current interaction g^4/(2^6 M_W^4) * (cosCabibbo)^2, ~MeV^-4
+        else if(process == 0){ FF = pow(4*Pi*Alpha/Q2,2); } //EM interaction e^4/Q^4, ~MeV^-4
+        
+        // // computing the cross section // //
+        double Ki_P = Ki[0]*P[0] - Ki[1]*P[1] - Ki[2]*P[2] - Ki[3]*P[3];
+        double KK = MN2/(abs(Ki_P) * Kf[0]*PN[0]) * FF; //~MeV^-6
+      
+        double frec = abs(pN/PN[0] - (pN-q*costheta_qN)/P[0]); //~dimensionless
+
+        // d5sig/(dkf dOmegaf dOmegaN)
+        double d5sig = 3/(4*Pi*pow(pF,3)) * KK * pow(pN*kf/(2*Pi),2) / frec * abs(LmnHmn); // ~MeV^-3 
+        
+        // // // // // // // // // // // // 
+        
+        // // integral over phiN // // 
+        if(phiN_deg==0 || phiN_deg==360){phiN_step=phiN_step/2;}
+        d4sig += d5sig * phiN_step;
+        // // // // // // // // // // 
+
+      } //phiN
+      
+      // // integral over phiN // //
+      if(thetaN_deg==0 || thetaN_deg==180){thetaN_step=thetaN_step/2;}
+      d3sig += d4sig * sinthetaN*thetaN_step;
+      // // // // // // // // // // 
+      
+    }//thetaN
+        
+
+    // // printing the output in a file (and in the screen) // //
+    if(d3sig>0)
+    {
+      
+      // // so far  d3sig is in 1/(MeV^3 srad)
+
+      d3sig = d3sig*pow(hbc,2);// hbc^2 to get the cross section in fm^2/(MeV srad)
+      
+//       double d2sig = d3sig * 2.*Pi; 
+      
+      out << w << " " << kf << " " << d3sig << endl; //d3s/(dkf dOmegaf) ; units~(fm^2/MeV srad)
+    }
+    
+    cout << w << " " << d3sig << endl;
+    // // // // // // // // // // // // // // 
+    
+  } //w
+  
+  return 0; 
+
+} //main closed
+
+
+
---
+++ CS-RFG_QE_c++/single-differential/formfactors.cpp
@@ -0,0 +1,71 @@
+#include "formfactors.h"
+
+void formfactors( double Qsq, double &F1p, double &F2p, double &F1n, double &F2n, double &GA, double &FP)
+{
+  
+  double QsqGeV, xmu, tau;
+  QsqGeV = Qsq/1.E6;
+  xmu = 2*MN;
+  tau = Qsq/(xmu*xmu);
+
+  double DipV, DipA, MA2, MV2;
+  MA2 = 1.065024; //MA=1.032 GeV
+  MV2 = 0.710649; //MV=0.843 GeV
+  DipV = 1./pow( 1+QsqGeV/MV2, 2 ); 
+  DipA = 1./pow( 1+QsqGeV/MA2, 2 ); 
+
+  double rmup=2.79285;
+  double rmun=-1.91304;
+
+  double GEp, GEn, GMp, GMn;
+  
+  int iff;
+//     iff = 0; // Dipole
+  iff = 1; // Kelly
+  
+  if(iff == 0){ //Dipole parametrization
+
+    GEp = DipV;
+    GMp = rmup*GEp;
+    GEn = -rmun*tau*GEp/(1.+5.6*tau);
+    GMn = rmun*GEp;
+
+  }else if(iff == 1){ //parametrization by Kelly Phys. Rev. C 70, 068202 (2004)
+
+    double As=1.70;
+    double Bs=3.30;
+
+    double a0=1.0;
+
+    double a1Gep=-0.24;
+    double b1Gep=10.98;
+    double b2Gep=12.82;
+    double b3Gep=21.97;
+
+    double a1Gmp=0.12;
+    double b1Gmp=10.97;
+    double b2Gmp=18.86;
+    double b3Gmp=6.55;
+    
+    double a1Gmn=2.33;
+    double b1Gmn=14.72;
+    double b2Gmn=24.20;
+    double b3Gmn=84.1;
+
+    GEn = As*tau/(1.+Bs*tau)*DipV;
+    GEp = (a0 + a1Gep*tau)/(1. + b1Gep*tau + b2Gep*pow(tau,2) + b3Gep*pow(tau,3));
+    GMp = rmup*(a0 + a1Gmp*tau)/(1. + b1Gmp*tau + b2Gmp*pow(tau,2) + b3Gmp*pow(tau,3));
+    GMn = rmun*(a0 + a1Gmn*tau)/(1. + b1Gmn*tau + b2Gmn*pow(tau,2) + b3Gmn*pow(tau,3));
+
+  }
+    
+  F1p = (GEp+tau*GMp)/(1.+tau);
+  F2p = (GMp-GEp)/(1.+tau);
+  F1n = (GEn+tau*GMn)/(1.+tau);
+  F2n = (GMn-GEn)/(1.+tau);
+
+  GA = -gA * DipA ;
+  FP = GA*4*MN/(Qsq + Mpi2);
+
+}
+
